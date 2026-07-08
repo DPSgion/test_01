8ad0db86e6e2343e6f99a2bdf148db569fff368e
Phuong <np384874@gmail.com>
2026-07-09 02:31:49 +0700

    Test with .env and .dockerignore

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
 const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.CUSTOM_MESSAGE || 'Gia tri mac dinh (chua nhan duoc env)';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from PaaS Test App!\n');
+  res.end(`${MESSAGE}\n`);
 });
 
 server.listen(PORT, () => {
